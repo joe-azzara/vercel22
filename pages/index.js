90a51d62fe43f126dd3b57cc49918dd2eaf56d1a
joeazzara <joe.azzara@gmail.com>
2023-06-12 20:40:38 -0400

    new name to blog 2

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://kc2ubg.com">My new Vercel Blog</a>
+          Welcome to <a href="https://kc2ubg.com">My New Blog</a>
         </h1>
 
         <p className={styles.description}>
