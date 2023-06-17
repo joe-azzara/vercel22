75a816b02194b6d2f6b53e38cb0f8f0012c1fbd1
joeazzara <joe.azzara@gmail.com>
2023-06-16 20:54:37 -0400

    nyc new new blog

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://kc2ubg.com">My New Blog</a>
+          Welcome to <a href="https://kc2ubg.com">My New New Blog</a>
         </h1>
 
         <p className={styles.description}>
